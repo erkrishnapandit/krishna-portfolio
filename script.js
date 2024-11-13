7929b51f0f1425fc5e90ad45e150f554219b930a
Krishna Kumar Pandit <krishnakumarji100@gmail.com>
2024-11-13 18:02:42 +0530

    About Section compleded

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-let tablinks = document.getElementById('tab-links');
-let tabcontent = document.getElementById('tab-contents');
+var tablinks = document.getElementsByClassName('tab-links');
+var tabcontents = document.getElementsByClassName('tab-content');
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
