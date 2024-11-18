5358f7f54191d1434549aa446a099d87686328c9
Krishna Kumar Pandit <krishnakumarji100@gmail.com>
2024-11-18 19:53:31 +0530

    Media Query Added for small screen

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var tablinks = document.getElementsByClassName('tab-links');
 var tabcontents = document.getElementsByClassName('tab-content');
-
+var sidemenu = document.getElementById('sidemenu');
 function opentab(tabname){
     for(tablink of tablinks){
         tablink.classList.remove("active-link");
@@ -12,4 +12,12 @@ function opentab(tabname){
 
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
+}
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-130px";
 }
